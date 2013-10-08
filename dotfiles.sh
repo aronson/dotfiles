3a84c8f44ba11d259aece8b351ddbe432692ed4e
Isaac C. Aronson <isaac@linux.com>
2013-10-08 14:52:49 -0400

    Change install script to not clobber on copy

--- dotfiles.sh
+++ dotfiles.sh
@@ -3,4 +3,4 @@
 SCRIPT=$(readlink -f "$0")
 DOTDIR=$(dirname "$SCRIPT")
 # cp files needed on most installs to $HOME
-cp -r $DOTDIR/{.zshrc,.zsh,.vimrc,.vim} $HOME
+cp -nr $DOTDIR/{.zshrc,.zsh,.vimrc,.vim} $HOME
