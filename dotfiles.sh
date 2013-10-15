42d47a00f2d828a915fedfcdfba3c59fdd197a8d
Isaac C. Aronson <isaac@linux.com>
2013-10-15 14:20:29 -0400

    Change cp behavior to force install with backup

--- dotfiles.sh
+++ dotfiles.sh
@@ -3,4 +3,4 @@
 SCRIPT=$(readlink -f "$0")
 DOTDIR=$(dirname "$SCRIPT")
 # cp files needed on most installs to $HOME
-cp -nr $DOTDIR/{.zshrc,.zsh,.vimrc,.vim} $HOME
+cp -bfr $DOTDIR/{.zshrc,.zsh,.vimrc,.vim,.vimpagerrc} $HOME
