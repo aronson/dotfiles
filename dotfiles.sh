5b79c3677eab3106c7836dcaba397d65c5b4d583
Isaac Aronson <i@pingas.org>
2021-07-16 12:02:53 -0500

    Add powerlevel10k configuration

--- dotfiles.sh
+++ dotfiles.sh
@@ -3,4 +3,4 @@
 SCRIPT=$(readlink -f "$0")
 DOTDIR=$(dirname "$SCRIPT")
 # cp files needed on most installs to $HOME
-ln -s $DOTDIR/{.zshrc,.zsh,.vimrc,.vim,.vimpagerrc} $HOME
+ln -s $DOTDIR/{.zshrc,.zsh,.vimrc,.vim,.vimpagerrc,.p10k.zsh} $HOME
